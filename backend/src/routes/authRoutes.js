64f8b572d6f4e66b6f099caf242a48ea233745fa
Sushmit0706 <sushmitsrivastava100@gmail.com>
2024-05-14 18:25:52 +0530

    error while running

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { logUserLogin } = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/login', logUserLogin);
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-
-
 import { Home, Product, Products, AboutPage, ContactPage, Cart, Login, Register, Checkout, PageNotFound } from "./pages"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
       <Routes>
@@ -27,5 +24,6 @@ root.render(
         <Route path="/product/*" element={<PageNotFound />} />
       </Routes>
     </Provider>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
