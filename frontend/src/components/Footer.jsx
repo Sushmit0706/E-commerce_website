314b654e1b38f1192c223082e20bc27947e4e3a2
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2024-05-18 00:31:14 +0530

    navbar changes (#1)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,18 +3,23 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="mb-0 text-center">
-        <div className="d-flex align-items-center justify-content-center pb-5">
-          <div className="col-md-6">
-            <p className="mb-3 mb-md-0">@2024 Kapda Lelo
-              {/* <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Kapda Lelo</a> */}
-            </p>
-            {/* <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+      <div className=" container">
+        <div className="w-full h-[100vh]  ">
+          <footer className="mb-0 text-center">
+            <div className="d-flex align-items-center justify-content-center pb-5">
+              <div className="col-md-6">
+                <p className="mb-3 mb-md-0">
+                  @2024 Kapda Lelo
+                  {/* <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Kapda Lelo</a> */}
+                </p>
+                {/* <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a> */}
-          </div>
+              </div>
+            </div>
+          </footer>
         </div>
-      </footer>
+      </div>
     </>
   );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,46 +1,81 @@
-import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../redux/action/authActions';
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../redux/action/authActions";
+import logo_light from "../assets/darkbag.png";
+import logo_dark from "../assets/lightbag.png";
+import toogle_light from "../assets/night.png";
+import toogle_dark from "../assets/day.png";
+import "../index.css";
 
-const Navbar = () => {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+const Navbar = ({ theme, setTheme }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const toggle_mode = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
   const handleLogout = () => {
     // Dispatch logout action
     dispatch(logout());
     // Redirect to home page
-    navigate('/');
+    navigate("/");
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
+    <nav className={`navbar ${theme} navbar-expand-lg py-3 sticky-top`}>
       <div className="container">
-        <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">Kapda lelo</NavLink>
-        <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <img
+          src={logo_light}
+          alt="Logo"
+          className="logo"
+          style={{ height: "60px" }}
+        />
+        <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">
+          Kapda lelo
+        </NavLink>
+        <button
+          className="navbar-toggler mx-2"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav m-auto my-2 text-center">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/">Home</NavLink>
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/product">Products</NavLink>
+              <NavLink className="nav-link" to="/product">
+                Products
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/about">About</NavLink>
+              <NavLink className="nav-link" to="/about">
+                About
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">Contact</NavLink>
+              <NavLink className="nav-link" to="/contact">
+                Contact
+              </NavLink>
             </li>
           </ul>
           <div className="buttons text-center">
             {isAuthenticated ? (
-              <button onClick={handleLogout} className="btn btn-outline-dark m-2">
+              <button
+                onClick={handleLogout}
+                className="btn btn-outline-dark m-2"
+              >
                 <i className="fa fa-sign-out-alt mr-1"></i> Logout
               </button>
             ) : (
@@ -56,6 +91,13 @@ const Navbar = () => {
             <NavLink to="/cart" className="btn btn-outline-dark m-2">
               <i className="fa fa-cart-shopping mr-1"></i> Cart
             </NavLink>
+            <img
+              onClick={toggle_mode}
+              src={theme === "light" ? toogle_light : toogle_dark}
+              alt="Toggle Theme"
+              // className="toggle-icon"
+              style={{ height: "40px" }}
+            />
           </div>
         </div>
       </div>
