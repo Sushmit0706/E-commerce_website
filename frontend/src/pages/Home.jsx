f06eb74728410396aeaeec126b1c0a8ee1cf9746
Yojana <tripathiyojana640@gmail.com>
2024-05-18 00:17:48 +0530

    navbar changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,17 @@
 import { Navbar, Main, Product, Footer } from "../components";
+import { useState } from "react";
+import "../index.css";
 
 function Home() {
+  const [theme, setTheme] = useState("light");
   return (
-    <>
-      <Navbar />
+    <div className={` ${theme}`}>
+      <Navbar theme={theme} setTheme={setTheme} />
       <Main />
       <Product />
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
